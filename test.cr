fff79595010b7fc016fccfdc33dc24dfefaa915c
FeepingCreature <default_357-line@yahoo.de>
2010-06-03 23:04:22 +0200

    Removed outdated proposal

--- test.cr
+++ test.cr
@@ -24,33 +24,6 @@ int acker(int m, int n) {
   } else return n + 1;
 }
 
-/*
-void test2(A a) {
-  printf("A test: %i\n", a.x);
-}
-
-class A {
-  int x;
-}
-*/
-
-/*
-  OO design rev1
-  
-*/
-
-/*extend {
-  bool gotExtC(ref string text, out NilStatement ns) {
-    ns = new NilStatement;
-    Type t;
-    string id;
-    return text.accept("extern(C)") && text.gotType(
-  }
-  this() {
-    
-  }
-}*/
-
 struct X {
   int a, b;
   int c;
