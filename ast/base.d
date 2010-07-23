df6fe13a3ffab79ecc7f29ddcf78719395cff2d6
FeepingCreature <default_357-line@yahoo.de>
2010-07-23 14:31:59 +0200

    Symbols should be void*

--- ast/base.d
+++ ast/base.d
@@ -110,7 +110,7 @@ class Symbol : Expr {
   string name;
   this(string name) { this.name = name; }
   mixin defaultIterate!();
-  override IType valueType() { return Single!(SysInt); }
+  override IType valueType() { return Single!(Pointer, Single!(Void)); }
   override void emitAsm(AsmFile af) {
     af.pushStack("$"~name, valueType());
   }
