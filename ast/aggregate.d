29f2aa4ac340447415bdf069da54e3edacbf373c
FeepingCreature <default_357-line@yahoo.de>
2010-07-25 21:43:52 +0200

    :: is now "rest-of-scope" statement

--- ast/aggregate.d
+++ ast/aggregate.d
@@ -22,3 +22,16 @@ Object gotAggregateStmt(ref string text, ParseCb cont, ParseCb rest) {
   else return null;
 }
 mixin DefaultParser!(gotAggregateStmt, "tree.stmt.aggregate");
+
+Object gotRestStmt(ref string text, ParseCb cont, ParseCb rest) {
+  auto t2 = text;
+  if (!t2.accept("::")) return null;
+  auto as = new AggrStatement;
+  Statement st;
+  t2.many(!!rest(t2, "tree.stmt", &st), { as.stmts ~= st; });
+  auto t3 = t2;
+  if (t3.strip().length) t3.mustAccept("}", "Unterminated rest statement: "~t3.next_text());
+  text = t2;
+  return as;
+}
+mixin DefaultParser!(gotRestStmt, "tree.stmt.rest_aggregate");
