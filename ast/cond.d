f466fa2c09e93b683b05951b1e0f80979bb35382
FeepingCreature <default_357-line@yahoo.de>
2010-11-08 13:30:12 +0100

    Cleaned up old comment/wip

--- ast/cond.d
+++ ast/cond.d
@@ -2,45 +2,8 @@
 module ast.cond;
 
 import
-  ast.base, ast.parse, ast.namespace, ast.oop, ast.tuples,
-  ast.literal_string, ast.arrays, ast.pointer;
-
-static int i;
-Expr genConvertClassExpr(Expr ex) {
-  /+auto cl = new Class("__convert_"~Format(i++)~"_for_"~ex.valueType().mangle(), null);
-  auto tuptype = cast(Tuple) ex.valueType();
-  if (tuptype) cl.iparents = [iparse!(IntfRef, "gencce_1", "type")
-                                     (`sys.ITupleValue`).myIntf].dup;
-  else cl.iparents = [iparse!(IntfRef, "gencce_2", "type")
-                             (`sys.IExprValue`).myIntf].dup;
-  auto exprs = getAllImplicitCasts(ex), tupex = mkTupleValueExpr(exprs);
-  cl.sup = namespace();
-  {
-    namespace.set(cl);
-    scope(exit) namespace.set(cl.sup);
-    new RelMember("tuple", tupex.valueType(), cl);
-    auto fun = iparse!(Function, "gencce_fun", "struct_fundef")
-                      (`sys.IExprValue take(string type, void* to) { }`);
-    auto sc = cast(Scope) fun.tree;
-    assert(sc);
-    {
-      namespace.set(sc);
-      scope(exit) namespace.set(cl);
-      sc.addStatement(
-        iparse!(Statement, "gencce_stmt", "tree.stmt")
-               (`if (type == typestring) { *cast(type*) { to = tuple[id]; return; }`, namespace(),
-                "type", exprs[0].valueType(),
-                "typestring", new StringExpr(exprs[0].valueType())));
-      assert(false);
-      /*void addSt(int id) {
-        auto type = exprs[id].valueType();
-      */
-    }
-  }+/
-  return null;
-}
-
-import ast.namespace, ast.modules, ast.vardecl, ast.variable, ast.scopes, ast.nestfun, ast.casting;
+  ast.base, ast.parse, ast.oop, ast.namespace, ast.modules, ast.vardecl,
+  ast.variable, ast.scopes, ast.nestfun, ast.casting, ast.arrays;
 // I'm sorry this is so ugly.
 Object gotHdlStmt(ref string text, ParseCb cont, ParseCb rest) {
   string t2 = text;
