b82f2b5f05ddcd427ee8e0144a3ede7459396e40
FeepingCreature <default_357-line@yahoo.de>
2010-11-09 06:03:11 +0100

    extern variables supported

--- ast/externs.d
+++ ast/externs.d
@@ -2,6 +2,25 @@ module ast.externs;
 
 import ast.base, ast.fun, ast.namespace;
 
+class ExternCGlobVar : Expr, Named {
+  IType type;
+  string name;
+  mixin defaultIterate!();
+  ExternCGlobVar dup() { return this; /* invariant */ }
+  this(IType t, string n) {
+    this.type = t;
+    this.name = n;
+  }
+  override {
+    IType valueType() { return type; }
+    string getIdentifier() { return name; }
+    void emitAsm(AsmFile af) {
+      af.pushStack(name, type);
+    }
+    string toString() { return Format("extern(C) global ", name, " of ", type); }
+  }
+}
+
 Object gotExtern(ref string text, ParseCb cont, ParseCb rest) {
   auto t2 = text;
   if (!t2.accept("extern(C)")) return null;
@@ -9,13 +28,27 @@ Object gotExtern(ref string text, ParseCb cont, ParseCb rest) {
     auto fun = new Function;
     fun.extern_c = true;
     New(fun.type);
-    if (test(fun.type.ret = cast(IType) rest(t2, "type")) &&
-        t2.gotIdentifier(fun.name) &&
-        t2.gotParlist(fun.type.params, rest) &&
-        t2.accept(";")
+    auto t3 = t2;
+    if (test(fun.type.ret = cast(IType) rest(t3, "type")) &&
+        t3.gotIdentifier(fun.name) &&
+        t3.gotParlist(fun.type.params, rest) &&
+        t3.accept(";")
       )
     {
+      t2 = t3;
       namespace().add(fun);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  bool grabVar() {
+    auto t3 = t2;
+    IType type; string name;
+    if (rest(t3, "type", &type) && t3.gotIdentifier(name) && t3.accept(";")) {
+      t2 = t3;
+      auto gv = new ExternCGlobVar(type, name);
+      namespace().add(gv);
       return true;
     } else {
       return false;
@@ -25,9 +58,9 @@ Object gotExtern(ref string text, ParseCb cont, ParseCb rest) {
     assert(false, "extern parsing failed at '"~t2.next_text()~"'.");
   }
   if (t2.accept("{")) {
-    while (grabFun()) { }
+    while (grabFun() || grabVar()) { }
     if (!t2.accept("}")) fail;
-  } else if (!grabFun()) fail;
+  } else if (!grabFun() && !grabVar()) fail;
   text = t2;
   return Single!(NoOp);
 }
