308ff7c5950583ab9ab7e6ac5d5044e97437e0a8
FeepingCreature <default_357-line@yahoo.de>
2010-11-21 16:57:25 +0100

    Use VBOs for newell teapot.

--- opengl.cr
+++ opengl.cr
@@ -2,3 +2,9 @@ module opengl;
 
 c_include "GL/gl.h";
 c_include "GL/glu.h";
+
+extern(C) void* glXGetProcAddress(char*);
+
+alias glBindBufferARB = *void function(GLenum, GLuint): glXGetProcAddress("glBindBufferARB");
+alias glBufferDataARB = *void function(GLenum, GLsizei, void*, GLenum): glXGetProcAddress("glBufferDataARB");
+alias glGenBuffersARB = *void function(GLsizei, GLuint*): glXGetProcAddress("glGenBuffersARB");
--- teapot.cr
+++ teapot.cr
@@ -14,32 +14,32 @@ int resizeWindow(int w, int h) {
     h = 1;
   auto ratio = w * 1.0 / h;
   glViewport(0, 0, w, h);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
+  glMatrixMode GL_PROJECTION;
+  glLoadIdentity;
   gluPerspective(45.0, ratio, 0.1, 100.0);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
+  glMatrixMode GL_MODELVIEW;
+  glLoadIdentity;
   return true;
 }
 
 int initGL() {
-  glShadeModel(GL_SMOOTH);
-  glClearColor(0, 0, 0, 0);
-  glClearDepth(1);
-  glEnable(GL_DEPTH_TEST);
-  glDepthFunc(GL_LEQUAL);
-  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+  glShadeModel GL_SMOOTH;
+  glClearColor (0 x 4);
+  glClearDepth 1;
+  glEnable GL_DEPTH_TEST;
+  glDepthFunc GL_LEQUAL;
+  glHint (GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
   return true;
 }
 
 context Triangles {
-  alias onUsing = glBegin(GL_TRIANGLES);
-  alias onExit = glEnd();
+  alias onUsing = glBegin GL_TRIANGLES;
+  alias onExit = glEnd;
 }
 
 context Quads {
-  alias onUsing = glBegin(GL_QUADS);
-  alias onExit = glEnd();
+  alias onUsing = glBegin GL_QUADS;
+  alias onExit = glEnd;
 }
 
 class DataSet {
@@ -77,6 +77,11 @@ DataSet parse(string fn) {
 int fps, last_time;
 float t;
 vec3f[] temp;
+
+GLuint teaObj;
+int entries;
+bool needUpdate;
+
 void drawScene(DataSet ds) {
   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glLoadIdentity;
@@ -86,49 +91,53 @@ void drawScene(DataSet ds) {
   glRotatef (t, 0, 1, 0);
   t -= 1;
   int vertices;
-  while auto ind <- zip (0..-1, ds.indices) {
-    float f = ind[0] * 1.0 / ds.indices.length;
-    glColor3f (f, f, f);
-    vec3f vcross(vec3f a, vec3f b) {
-      return vec3f(a[1]*b[2] - b[1]*a[2], a[2]*b[0] - b[2]*a[0], a[0]*b[1] - b[0]*a[1]);
-    }
-    float vdot(vec3f a, vec3f b) {
-      return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
-    }
-    float vlength(vec3f a) {
-      return sqrtf(vdot(a, a));
-    }
-    vec3f vnormal(vec3f a) {
-      return a / vlength(a);
-    }
-    vec3f blend(vec3f from, vec3f to, float u) {
-      return from + (to - from) * u;
-    }
-    void bezier2(float u, vec3f[] field, vec3f* dest) {
-      vec3f[5] temp = void;
-      temp[0] = blend(field[0], field[1], u);
-      temp[1] = blend(field[1], field[2], u);
-      temp[2] = blend(field[2], field[3], u);
-      temp[3] = blend(temp[0], temp[1], u);
-      temp[4] = blend(temp[1], temp[2], u);
-      *dest = blend(temp[3], temp[4], u);
-    }
-    vec3f[16] input = void;
-    while auto k <- 0..16 {
-      input[k] = ds.vecs[ind[1][k] - 1];
-    }
-    vec3f[4] bezier_temp = void;
-    void setup_row(float v) {
-      bezier2(v, input[0..4], &bezier_temp[0]);
-      bezier2(v, input[4..8], &bezier_temp[1]);
-      bezier2(v, input[8..12], &bezier_temp[2]);
-      bezier2(v, input[12..16], &bezier_temp[3]);
-    }
-    void bezier3(float u, vec3f* dest) {
-      bezier2(u, bezier_temp[], dest);
-    }
-    using Quads {
-      alias subdiv = 8;
+  if needUpdate {
+    needUpdate = false;
+    entries = 0;
+    vec3f[auto~] data;
+    onExit { data.free; }
+    while auto ind <- zip (0..-1, ds.indices) {
+      float f = ind[0] * 1.0 / ds.indices.length;
+      glColor3f (f, f, f);
+      vec3f vcross(vec3f a, vec3f b) {
+        return vec3f(a[1]*b[2] - b[1]*a[2], a[2]*b[0] - b[2]*a[0], a[0]*b[1] - b[0]*a[1]);
+      }
+      float vdot(vec3f a, vec3f b) {
+        return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
+      }
+      float vlength(vec3f a) {
+        return sqrtf(vdot(a, a));
+      }
+      vec3f vnormal(vec3f a) {
+        return a / vlength(a);
+      }
+      vec3f blend(vec3f from, vec3f to, float u) {
+        return from + (to - from) * u;
+      }
+      void bezier2(float u, vec3f[] field, vec3f* dest) {
+        vec3f[5] temp = void;
+        temp[0] = blend(field[0], field[1], u);
+        temp[1] = blend(field[1], field[2], u);
+        temp[2] = blend(field[2], field[3], u);
+        temp[3] = blend(temp[0], temp[1], u);
+        temp[4] = blend(temp[1], temp[2], u);
+        *dest = blend(temp[3], temp[4], u);
+      }
+      vec3f[16] input = void;
+      while auto k <- 0..16 {
+        input[k] = ds.vecs[ind[1][k] - 1];
+      }
+      vec3f[4] bezier_temp = void;
+      void setup_row(float v) {
+        bezier2(v, input[0..4], &bezier_temp[0]);
+        bezier2(v, input[4..8], &bezier_temp[1]);
+        bezier2(v, input[8..12], &bezier_temp[2]);
+        bezier2(v, input[12..16], &bezier_temp[3]);
+      }
+      void bezier3(float u, vec3f* dest) {
+        bezier2(u, bezier_temp[], dest);
+      }
+      alias subdiv = 256;
       int subdivp = subdiv + 1;
       if (!temp.ptr) temp = new vec3f[subdivp * subdivp];
       int k;
@@ -163,11 +172,20 @@ void drawScene(DataSet ds) {
           if (angle < 0) angle = -angle;
           angles[l] = angle;
         }
-        while int i <- 0..4 { glColor3f vec3f(angles[i]); glVertex3f quad[i]; }
+        while int i <- 0..4 { data ~= vec3f(angles[i]); data ~= quad[i]; entries ++; }
         vertices += 4;
       }
     }
+    glBindBufferARB(GL_ARRAY_BUFFER_ARB, teaObj);
+    glBufferDataARB(GL_ARRAY_BUFFER_ARB, (size-of vec3f) * data.length, data.ptr, GL_STATIC_DRAW_ARB);
   }
+  glEnableClientState GL_COLOR_ARRAY;
+  glEnableClientState GL_VERTEX_ARRAY;
+  glBindBufferARB(GL_ARRAY_BUFFER_ARB, teaObj);
+  glColorPointer(3, GL_FLOAT, 2 * size-of vec3f, void*: 0);
+  glVertexPointer(3, GL_FLOAT, 2 * size-of vec3f, void*: size-of vec3f);
+  // writeln "Render $entries entries. ";
+  glDrawArrays (GL_QUADS, 0, entries);
   SDL_GL_SwapBuffers();
   fps ++;
   auto ct = time(time_t*:null);
@@ -189,6 +207,8 @@ int update(SDL_Surface* surface) {
 
 int main(int argc, char** argv) {
   t = 0;
+  needUpdate = true;
+  
   auto ds = parse "newell_teaset/teapot";
   SDL_Init (SDL_INIT_VIDEO);
   auto flags = SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE;
@@ -196,6 +216,8 @@ int main(int argc, char** argv) {
   auto surf = SDL_SetVideoMode (640, 480, 0, flags);
   if !surf quit(1);
   initGL;
+  // IMPORTANT: init gl FIRST
+  glGenBuffersARB (1, &teaObj);
   resizeWindow (640, 480);
   while true {
     drawScene(ds);
