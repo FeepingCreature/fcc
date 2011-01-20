c512f78dd01a48d7e36ff5d147643b085ddd3ed6
FeepingCreature <default_357-line@yahoo.de>
2011-01-20 18:17:50 +0100

    Added argc, argv supported main form.

--- ast/main.d
+++ ast/main.d
@@ -1,7 +1,7 @@
 module ast.main;
 
 import ast.base, ast.fun, ast.intrinsic, ast.modules, ast.namespace;
-import ast.scopes, ast.arrays, ast.returns, ast.parse;
+import ast.scopes, ast.arrays, ast.returns, ast.parse, ast.pointer;
 
 void fixupMain() {
   auto cmain = cast(Function) sysmod.lookup("_c_main");
@@ -10,6 +10,7 @@ void fixupMain() {
   if (!sc) { logln("fail 1: ", cmain.tree); fail(); }
   auto argvar = cast(Expr) sc.lookup("args");
   if (!argvar) { logln("fail 2: ", sc.field); fail(); }
+  auto cvar = cast(Expr) sc.lookup("argc"), pvar = cast(Expr) sc.lookup("argv");
   if (!gotMain) {
     logln("main function not found! ");
     fail();
@@ -17,24 +18,35 @@ void fixupMain() {
   auto call = gotMain.mkCall();
   
   auto m = gotMain;
-  bool mainReturnsInt, mainTakesStrings;
-  if (m.type.ret == Single!(SysInt))
-    mainReturnsInt = true;
-  if (!m.type.params.length) { }
-  else {
-    if (m.type.params.length != 1) {
-      logln("invalid main form: ", m.type);
-      fail();
-    }
-    if (m.type.params[0]._0 == Single!(Array, Single!(Array, Single!(Char))))
-      mainTakesStrings = true;
+  bool mainReturnsInt, mainTakesStrings, mainTakesArgCV; // argc, argv
+  with (m.type) {
+    if (ret == Single!(SysInt))
+      mainReturnsInt = true;
+    if (!params.length) { }
     else {
-      logln("invalid main form: ", m.type);
-      fail();
+      if (params.length == 2) {
+        if (params[0]._0 == Single!(SysInt) && params[1]._0 == Single!(Pointer, Single!(Pointer, Single!(Char))))
+          mainTakesArgCV = true;
+        else {
+          logln("invalid main form (1): ", m.type);
+          fail();
+        }
+      } else if (params.length == 1) {
+        if (params[0]._0 == Single!(Array, Single!(Array, Single!(Char))))
+          mainTakesStrings = true;
+        else {
+          logln("invalid main form (2): ", m.type);
+          fail();
+        }
+      } else {
+        logln("invalid main form (3): ", m.type);
+        fail();
+      }
     }
   }
   
   if (mainTakesStrings) call.params ~= argvar;
+  if (mainTakesArgCV) call.params ~= [cvar, pvar];
   Statement res;
   
   auto backup = namespace();
