7d7a85118921838280b7054475fa41f239a06f99
FeepingCreature <default_357-line@yahoo.de>
2011-02-15 12:28:29 +0100

    WIP threadpool support

--- std/thread.cr
+++ std/thread.cr
@@ -15,3 +15,87 @@ void startThread(void delegate() dg) {
   *argp = dg;
   pthread_create(&buf, null, &start_routine, argp);
 }
+
+struct pthread_mutex_t { ubyte[40] filler; }
+extern(C) int pthread_mutex_lock (pthread_mutex_t*);
+extern(C) int pthread_mutex_unlock (pthread_mutex_t*);
+
+class Mutex {
+  pthread_mutex_t mutex;
+  void lock() { pthread_mutex_lock &mutex; }
+  void unlock() { pthread_mutex_unlock &mutex; }
+}
+
+struct MutexWrapper {
+  Mutex m;
+  void onUsing() { m.lock(); }
+  void onExit() { m.unlock(); }
+}
+
+MutexWrapper autoLock(Mutex m) { MutexWrapper mw; mw.m = m; return mw; }
+
+c_include "semaphore.h";
+class Semaphore {
+  sem_t hdl;
+  void init() { sem_init(&hdl, false, 0); }
+  void claim() { sem_wait(&hdl); }
+  void release() { sem_post(&hdl); }
+}
+
+class ThreadPool {
+  Mutex m;
+  Semaphore s, t;
+  int tasksLeft;
+  void delegate()[auto~] queue;
+  void threadFun() {
+    while (true) {
+      s.claim;
+      void delegate() dg;
+      /*using autoLock m {
+        writeln "got dg? ";
+        dg = queue[0];
+        queue = type-of queue:queue[1 .. $];
+      }*/
+      m.lock;
+      dg = queue[0];
+      queue = type-of queue:queue[1 .. $];
+      m.unlock;
+      dg();
+      // using autoLock m { t.release(); }
+      m.lock;
+      t.release;
+      m.unlock;
+    }
+  }
+  void addThread() {
+    startThread &threadFun;
+  }
+  void waitComplete() {
+    int i;
+    // using autoLock(m) { i = tasksLeft; tasksLeft = 0; }
+    m.lock;
+    i = tasksLeft;
+    tasksLeft = 0;
+    m.unlock;
+    while 0..i t.claim();
+  }
+  void addTask(void delegate() dg) {
+    // using autoLock(m) { queue ~= dg; tasksLeft ++; }
+    MutexWrapper mw; mw.m = m;
+    eval (mw.onUsing);
+    // m.lock;
+    queue ~= dg;
+    tasksLeft ++;
+    s.release;
+    eval (mw.onExit);
+    // m.unlock;
+  }
+}
+
+ThreadPool mkThreadPool(int threads = 0) using new ThreadPool {
+  m = new Mutex;
+  s = new Semaphore; t = new Semaphore;
+  s.init; t.init;
+  for 0..threads addThread();
+  return this;
+}
--- threads.cr
+++ threads.cr
@@ -3,7 +3,8 @@ module threads;
 import std.thread;
 
 void main() {
+  auto tp = mkThreadPool(4);
   for (int i <- 0..10) 
-    startThread new delegate void() { writeln "$i"; };
-  while (true) { }
+    tp.addTask new delegate void() { writeln "$i"; };
+  tp.waitComplete;
 }
