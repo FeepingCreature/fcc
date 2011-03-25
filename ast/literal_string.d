d570c855b79f27d18c1436daaac4ceb39f56baba
FeepingCreature <default_357-line@yahoo.de>
2011-03-25 18:58:42 +0100

    Added support for import()ing files as arrays. TODO: move into separate module.

--- ast/literal_string.d
+++ ast/literal_string.d
@@ -88,3 +88,24 @@ static this() {
     return null;
   };
 }
+
+import std.file, ast.static_arrays;
+Object gotImportFile(ref string text, ParseCb cont, ParseCb rest) {
+  auto t2 = text;
+  Expr filename;
+  if (!rest(t2, "tree.expr", &filename)) {
+    t2.failparse("Couldn't parse file name expression");
+  }
+  if (!t2.accept(")"))
+    t2.failparse("Expected closing paren");
+  filename = foldex(filename);
+  auto se = fastcast!(StringExpr) (filename);
+  if (!se)
+    text.failparse("Expected string expr, got ", (cast(Object) filename).classinfo.name);
+  auto data = read(se.str);
+  auto res = new DataExpr;
+  res.data = cast(ubyte[]) data;
+  text = t2;
+  return res;
+}
+mixin DefaultParser!(gotImportFile, "tree.expr.import_file", "24065", "import(");
