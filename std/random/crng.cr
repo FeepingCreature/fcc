c72f851dc224f17c60cae7ac6347809991fff192
FeepingCreature <default_357-line@yahoo.de>
2011-04-26 12:23:07 +0200

    Windows has no rand_r :(

--- std/random/crng.cr
+++ std/random/crng.cr
@@ -2,15 +2,19 @@ module std.random.crng;
 
 import std.random.base, std.c.stdlib;
 
-class CRNG : IRandom {
-  int seed;
-  void init(int s) { seed = s; }
-  int rand() { return rand_r &seed; }
-}
+// no rand_r on windows, fo shame. 
+platform(default) <<EOF
 
-void init() {
-  engines ~= (
-    delegate IRandom(int s) { return new CRNG s; },
-    delegate IRandom(IRandom ir) { return new CRNG ir.rand(); },
-    1); // low-qual C RNG
-}
+  class CRNG : IRandom {
+    int seed;
+    void init(int s) { seed = s; }
+    int rand() { return rand_r &seed; }
+  }
+  
+  void init() {
+    engines ~= (
+      delegate IRandom(int s) { return new CRNG s; },
+      delegate IRandom(IRandom ir) { return new CRNG ir.rand(); },
+      1); // low-qual C RNG
+  }
+EOF
