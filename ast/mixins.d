0c7863b7ec033b7e4e8f731e4de46757ef5b9ce1
FeepingCreature <default_357-line@yahoo.de>
2011-07-31 01:42:58 +0200

    Allow mixin() to yield statements, not just expressions

--- ast/mixins.d
+++ ast/mixins.d
@@ -15,12 +15,35 @@ Object gotMixinExpr(ref string text, ParseCb cont, ParseCb rest) {
   auto se = fastcast!(StringExpr) (foldex(ex2));
   auto src = se.str;
   Object res;
+  pushCache(); scope(exit) popCache();
   if (!rest(src, "tree.expr", &res))
-    src.failparse("Couldn't parse mixin string");
+    src.failparse("Couldn't parse mixin string for expr");
   if (src.length)
-    src.failparse("Unknown text found. ");
+    src.failparse("Unknown text found for expr. ");
   text = t2;
   return res;
 }
-
 mixin DefaultParser!(gotMixinExpr, "tree.expr.mixin", "222", "mixin");
+
+Object gotMixinStmt(ref string text, ParseCb cont, ParseCb rest) {
+  auto t2 = text;
+  Expr ex;
+  if (!rest(t2, "tree.expr", &ex)) 
+    t2.failparse("Couldn't match mixin string! ");
+  auto ex2 = ex;
+  if (!gotImplicitCast(ex2, (Expr ex) {
+    return !!fastcast!(StringExpr) (foldex(ex));
+  }))
+    t2.failparse("Couldn't mixin: Not a string constant: ", ex);
+  auto se = fastcast!(StringExpr) (foldex(ex2));
+  auto src = se.str;
+  Object res;
+  pushCache(); scope(exit) popCache();
+  if (!rest(src, "tree.stmt", &res))
+    src.failparse("Couldn't parse mixin string for stmt");
+  if (src.length)
+    src.failparse("Unknown text found for stmt. ");
+  text = t2;
+  return res;
+}
+mixin DefaultParser!(gotMixinStmt, "tree.semicol_stmt.mixin", "100", "mixin");
