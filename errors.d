bc4d0be2ae248d928aa79a03ba70b87c90d8247a
FeepingCreature <default_357-line@yahoo.de>
2011-09-07 01:30:15 +0200

    When giving an error message, mark the position of the error in the source line

--- errors.d
+++ errors.d
@@ -34,7 +34,7 @@ Stuple!(float, string) lookupProgress(string text) {
 }
 
 // row, col, file
-Stuple!(int, ptrdiff_t, string) lookupPos(string text) {
+Stuple!(int, ptrdiff_t, string, string) lookupPos(string text) {
   eatComments(text);
   text = text.strip();
   synchronized(SyncObj!(sourcefiles)) foreach (key, value; sourcefiles) {
@@ -47,11 +47,11 @@ Stuple!(int, ptrdiff_t, string) lookupPos(string text) {
         i++;
         continue;
       }
-      return stuple(i + 1, text.ptr - line.ptr, key.dup);
+      return stuple(i + 1, text.ptr - line.ptr, key.dup, line);
     }
     assert(false);
   }
-  return stuple(0, cast(ptrdiff_t) 0, "<unknown>");
+  return stuple(0, cast(ptrdiff_t) 0, "<unknown>", cast(string) null);
 }
 
 class ParseEx : Exception {
@@ -62,7 +62,16 @@ class ParseEx : Exception {
   string toString() {
     auto info = lookupPos(pos);
     if (info._2 == "<unknown>") info._2 = "@`"~pos.nextText()~"`";
-    auto res = Format(info._2, ":", info._0, ":", info._1, ": ", msg);
+    string res;
+    if (info._3) {
+      auto prefix = "At line: ";
+      res = Format(prefix, info._3, "\n");
+      for (int i = 0; i < prefix.length + info._1; ++i)
+        res ~= " ";
+      res ~= "^\n";
+    }
+    
+    res ~= Format(info._2, ":", info._0, ":", info._1, ": ", msg);
     if (rules) res ~= Format(" ", rules);
     return res;
   } 
