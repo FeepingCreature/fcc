197863290eaedc5227be73c78fb50cebc66713af
FeepingCreature <default_357-line@yahoo.de>
2011-11-16 18:08:40 +0100

    Separate root context for faster parsing, nil

--- ast/macros.d
+++ ast/macros.d
@@ -18,6 +18,8 @@ void tnte(T...)(T t) {
 class Context {
   Entity[string] defs;
   Context sup;
+  this() { }
+  this(Context c) { sup = c; }
   Entity lookup(string s) {
     if (auto p = s in defs) return *p;
     if (sup) return sup.lookup(s);
@@ -64,7 +66,7 @@ class List : Entity {
   string toString() { string res; foreach (i, ent; entries) { if (i) res ~= " "; res ~= Format(ent); } return "(" ~ res ~ ")"; }
   override Entity eval(Context ctx) {
     if (!entries.length)
-      return this;
+      tnte("Cannot evaluate empty list. ");
     
     auto first = entries[0].eval(ctx);
     auto firstc = fastcast!(Callable) (first);
@@ -186,22 +188,150 @@ class TenthMacro : NoOp, Named {
   this(Entity e) { root = e; identifier = Format("__tenth_macro_", macrocount++); }
 }
 
+Context rootctx;
+void initTenth() {
+  if (rootctx) return;
+  rootctx = new Context;
+  rootctx.add("nil", NilEnt);
+  rootctx.add("t", NonNilEnt);
+  rootctx.add("last", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    return args[$-1];
+  }));
+  rootctx.add("def", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 2) tnte("Too many arguments to def: 2 expected");
+    mixin(chaincast("tok: First arg to def: args[0]->Token: %.name"));
+    ctx.add(tok, args[1]);
+    return args[1];
+  }));
+  rootctx.add("make-tuple-expr", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 1) tnte("Wrong number of args to make-tuple-expr: 1 expected");
+    mixin(chaincast("list: Argument to make-tuple-expr: args[0]->List"));
+    Expr[] exlist = new Expr[list.entries.length];
+    foreach (i, e; list.entries) {
+      auto te = fastcast!(TreeEntity) (e);
+      if (!te) tnte("List for make-tuple-expr must contain Exprs, not ", e);
+      auto ex = fastcast!(Expr) (te.tr);
+      if (!ex) tnte("List for make-tuple-expr must contain Exprs, not ", te.tr);
+      exlist[i] = ex;
+    }
+    return new TreeEntity(mkTupleExpr(exlist));
+  }));
+  rootctx.add("make-call", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 2) tnte("Wrong number of args to make-call: 2 expected");
+    mixin(chaincast("fun: First arg for make-call: args[0]->TreeEntity: %.tr->Function"));
+    mixin(chaincast("ex: Second arg for make-call: args[1]->TreeEntity: %.tr->Expr"));
+    return cast(Entity) (new TreeEntity(buildFunCall(fun, ex, "tenth-call")));
+  }));
+  rootctx.add("lookup", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 1) tnte("Wrong number of args to lookup: 1 expected");
+    mixin(chaincast("tok: Arg for lookup: args[0]->Token: %.name"));
+    auto res = fastcast!(Tree) (namespace().lookup(tok));
+    if (!res) tnte("No such object: ", tok);
+    return new TreeEntity(res);
+  }));
+  rootctx.add("not", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 1) tnte("Wrong number of args to 'not': 1 expected");
+    if (isNil(args[0])) return NonNilEnt;
+    else return NilEnt;
+  }));
+  rootctx.add("if", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 3) tnte("Wrong number of args to 'if': 3 expected");
+    auto cond = args[0];
+    bool bcond = !isNil(cond);
+    if (bcond) return args[1].eval(ctx);
+    else return args[2].eval(ctx);
+  }));
+  rootctx.add("make-temporary", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 1) tnte("Wrong number of args to 'make-temporary': 1 expected");
+    mixin(chaincast("ty: Arg for make-temporary: args[0]->TypeEntity: %.ty"));
+    auto var = new Variable(ty, null, boffs(ty));
+    var.dontInit = true;
+    auto sc = namespace().get!(Scope);
+    sc.add(var);
+    sc.addStatement(new VarDecl(var));
+    return new TreeEntity(var);
+  }));
+  rootctx.add("type-of", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 1) tnte("Wrong number of args to 'type-of': 1 expected");
+    mixin(chaincast("ty: Arg for type-of: args[0]->TreeEntity: %.tr->Expr: %.valueType()"));
+    return new TypeEntity(ty);
+  }));
+  rootctx.add("make-sae", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 2) tnte("Wrong number of args to 'make-sae': 2 expected");
+    mixin(chaincast("st: First arg for make-sae: args[0]->TreeEntity: %.tr->Statement"));
+    mixin(chaincast("ex: Second arg for make-sae: args[1]->TreeEntity: %.tr->Expr"));
+    return new TreeEntity(mkStatementAndExpr(st, ex));
+  }));
+  rootctx.add("make-aggregate", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 1) tnte("Wrong number of args to 'make-aggregate': 1 expected");
+    mixin(chaincast("list: Arg for make-aggregate: args[0]->List"));
+    auto res = new AggrStatement;
+    foreach (ent; list.entries) {
+      mixin(chaincast("st: List entry for make-aggregate: ent->TreeEntity: %.tr->Statement"));
+      res.stmts ~= st;
+    }
+    return new TreeEntity(res);
+  }));
+  rootctx.add("make-assignment", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 2) tnte("Wrong number of args to 'make-assignment': 2 expected");
+    mixin(chaincast("dest: First arg for make-assignment: args[0]->TreeEntity: %.tr->Expr"));
+    mixin(chaincast("src: Second arg for make-assignment: args[1]->TreeEntity: %.tr->Expr"));
+    return new TreeEntity(mkAssignment(dest, src));
+  }));
+  rootctx.add("make-tuple-index", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 2) tnte("Wrong number of args to 'make-tuple-index': 2 expected");
+    mixin(chaincast("tup: First arg for make-tuple-index: args[0]->TreeEntity: %.tr->Expr"));
+    mixin(chaincast("index: Second arg for make-tuple-index: args[1]->Integer: %.value"));
+    return new TreeEntity(mkTupleIndexAccess(tup, index));
+  }));
+  rootctx.add("list", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    return new List(args);
+  }));
+  rootctx.add("tuple-length", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 1) tnte("Wrong number of args to 'tuple-length': 1 expected");
+    mixin(chaincast("len: Arg to 'tuple-length': args[0]->TreeEntity: %.tr->Expr: %.valueType()->Tuple: %.types.length"));
+    return new Integer(len);
+  }));
+  rootctx.add("for", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 3) tnte("Wrong number of args to 'for': 3 expected");
+    mixin(chaincast("range: First arg to 'for': args[0]->List"));
+    if (range.entries.length != 2) tnte("Wrong number of range parts to 'for': 2 expected");
+    mixin(chaincast("from: Lower end of range to 'for': range.entries[0]->Integer: %.value"));
+    mixin(chaincast("  to: Upper end of range to 'for': range.entries[1]->Integer: %.value"));
+    mixin(chaincast("ident: Second arg to 'for': args[1]->Token: %.name"));
+    auto loopct = new Context;
+    loopct.sup = ctx;
+    Entity[] res = new Entity[to-from];
+    for (int i = from; i < to; ++i) {
+      loopct.add(ident, new Integer(i));
+      res[i] = args[2].eval(loopct);
+    }
+    return new List(res);
+  }));
+  rootctx.add("map-tuple", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
+    if (args.length != 3) tnte("Wrong number of args to map-tuple: 3 expected");
+    mixin(chaincast("tup: First arg for map-tuple: args[0]->TreeEntity: %.tr->Expr"));
+    mixin(chaincast("ident: Second arg for map-tuple: args[1]->Token: %.name"));
+    auto entries = getTupleEntries(tup);
+    Entity[] res;
+    auto loopct = new Context;
+    loopct.sup = ctx;
+    foreach (ex; entries) {
+      loopct.add(ident, new TreeEntity(ex));
+      res ~= args[2].eval(loopct);
+    }
+    return new List(res);
+  }));
+}
+
 Object runTenth(Object obj, ref string text, ParseCb cont, ParseCb rest) {
   auto t2 = text;
   auto mac = fastcast!(TenthMacro) (obj);
   auto findme = namespace().lookup(mac.identifier, false);
   if (findme !is mac) return null; // check if we're in scope
   auto ent = mac.root;
-  auto ctx = new Context;
-  ctx.add("last", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    return args[$-1];
-  }));
-  ctx.add("def", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 2) tnte("Too many arguments to def: 2 expected");
-    mixin(chaincast("tok: First arg to def: args[0]->Token: %.name"));
-    ctx.add(tok, args[1]);
-    return args[1];
-  }));
+  initTenth;
+  auto ctx = new Context(rootctx);
   ctx.add("parse-ident", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
     if (args.length) tnte("Too many arguments to parse-ident: 0 expected");
     string ident;
@@ -224,125 +354,6 @@ Object runTenth(Object obj, ref string text, ParseCb cont, ParseCb rest) {
       return NilEnt;
     }
     return NonNilEnt;
-  }));
-  ctx.add("make-tuple-expr", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 1) tnte("Wrong number of args to make-tuple-expr: 1 expected");
-    mixin(chaincast("list: Argument to make-tuple-expr: args[0]->List"));
-    Expr[] exlist = new Expr[list.entries.length];
-    foreach (i, e; list.entries) {
-      auto te = fastcast!(TreeEntity) (e);
-      if (!te) tnte("List for make-tuple-expr must contain Exprs, not ", e);
-      auto ex = fastcast!(Expr) (te.tr);
-      if (!ex) tnte("List for make-tuple-expr must contain Exprs, not ", te.tr);
-      exlist[i] = ex;
-    }
-    return new TreeEntity(mkTupleExpr(exlist));
-  }));
-  ctx.add("make-call", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 2) tnte("Wrong number of args to make-call: 2 expected");
-    mixin(chaincast("fun: First arg for make-call: args[0]->TreeEntity: %.tr->Function"));
-    mixin(chaincast("ex: Second arg for make-call: args[1]->TreeEntity: %.tr->Expr"));
-    return cast(Entity) (new TreeEntity(buildFunCall(fun, ex, "tenth-call")));
-  }));
-  ctx.add("lookup", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 1) tnte("Wrong number of args to lookup: 1 expected");
-    mixin(chaincast("tok: Arg for lookup: args[0]->Token: %.name"));
-    auto res = fastcast!(Tree) (namespace().lookup(tok));
-    if (!res) tnte("No such object: ", tok);
-    return new TreeEntity(res);
-  }));
-  ctx.add("not", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 1) tnte("Wrong number of args to 'not': 1 expected");
-    if (isNil(args[0])) return NonNilEnt;
-    else return NilEnt;
-  }));
-  ctx.add("if", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 3) tnte("Wrong number of args to 'if': 3 expected");
-    auto cond = args[0];
-    bool bcond = !isNil(cond);
-    if (bcond) return args[1].eval(ctx);
-    else return args[2].eval(ctx);
-  }));
-  ctx.add("make-temporary", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 1) tnte("Wrong number of args to 'make-temporary': 1 expected");
-    mixin(chaincast("ty: Arg for make-temporary: args[0]->TypeEntity: %.ty"));
-    auto var = new Variable(ty, null, boffs(ty));
-    var.dontInit = true;
-    auto sc = namespace().get!(Scope);
-    sc.add(var);
-    sc.addStatement(new VarDecl(var));
-    return new TreeEntity(var);
-  }));
-  ctx.add("type-of", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 1) tnte("Wrong number of args to 'type-of': 1 expected");
-    mixin(chaincast("ty: Arg for type-of: args[0]->TreeEntity: %.tr->Expr: %.valueType()"));
-    return new TypeEntity(ty);
-  }));
-  ctx.add("make-sae", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 2) tnte("Wrong number of args to 'make-sae': 2 expected");
-    mixin(chaincast("st: First arg for make-sae: args[0]->TreeEntity: %.tr->Statement"));
-    mixin(chaincast("ex: Second arg for make-sae: args[1]->TreeEntity: %.tr->Expr"));
-    return new TreeEntity(mkStatementAndExpr(st, ex));
-  }));
-  ctx.add("make-aggregate", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 1) tnte("Wrong number of args to 'make-aggregate': 1 expected");
-    mixin(chaincast("list: Arg for make-aggregate: args[0]->List"));
-    auto res = new AggrStatement;
-    foreach (ent; list.entries) {
-      mixin(chaincast("st: List entry for make-aggregate: ent->TreeEntity: %.tr->Statement"));
-      res.stmts ~= st;
-    }
-    return new TreeEntity(res);
-  }));
-  ctx.add("make-assignment", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 2) tnte("Wrong number of args to 'make-assignment': 2 expected");
-    mixin(chaincast("dest: First arg for make-assignment: args[0]->TreeEntity: %.tr->Expr"));
-    mixin(chaincast("src: Second arg for make-assignment: args[1]->TreeEntity: %.tr->Expr"));
-    return new TreeEntity(mkAssignment(dest, src));
-  }));
-  ctx.add("make-tuple-index", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 2) tnte("Wrong number of args to 'make-tuple-index': 2 expected");
-    mixin(chaincast("tup: First arg for make-tuple-index: args[0]->TreeEntity: %.tr->Expr"));
-    mixin(chaincast("index: Second arg for make-tuple-index: args[1]->Integer: %.value"));
-    return new TreeEntity(mkTupleIndexAccess(tup, index));
-  }));
-  ctx.add("list", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    return new List(args);
-  }));
-  ctx.add("tuple-length", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 1) tnte("Wrong number of args to 'tuple-length': 1 expected");
-    mixin(chaincast("len: Arg to 'tuple-length': args[0]->TreeEntity: %.tr->Expr: %.valueType()->Tuple: %.types.length"));
-    return new Integer(len);
-  }));
-  ctx.add("for", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 3) tnte("Wrong number of args to 'for': 3 expected");
-    mixin(chaincast("range: First arg to 'for': args[0]->List"));
-    if (range.entries.length != 2) tnte("Wrong number of range parts to 'for': 2 expected");
-    mixin(chaincast("from: Lower end of range to 'for': range.entries[0]->Integer: %.value"));
-    mixin(chaincast("  to: Upper end of range to 'for': range.entries[1]->Integer: %.value"));
-    mixin(chaincast("ident: Second arg to 'for': args[1]->Token: %.name"));
-    auto loopct = new Context;
-    loopct.sup = ctx;
-    Entity[] res = new Entity[to-from];
-    for (int i = from; i < to; ++i) {
-      loopct.add(ident, new Integer(i));
-      res[i] = args[2].eval(loopct);
-    }
-    return new List(res);
-  }));
-  ctx.add("map-tuple", new DgCallable(delegate Entity(Context ctx, Entity[] args) {
-    if (args.length != 3) tnte("Wrong number of args to map-tuple: 3 expected");
-    mixin(chaincast("tup: First arg for map-tuple: args[0]->TreeEntity: %.tr->Expr"));
-    mixin(chaincast("ident: Second arg for map-tuple: args[1]->Token: %.name"));
-    auto entries = getTupleEntries(tup);
-    Entity[] res;
-    auto loopct = new Context;
-    loopct.sup = ctx;
-    foreach (ex; entries) {
-      loopct.add(ident, new TreeEntity(ex));
-      res ~= args[2].eval(loopct);
-    }
-    return new List(res);
   }));
   auto res = ent.eval(ctx);
   if (isNil(res)) return null;
