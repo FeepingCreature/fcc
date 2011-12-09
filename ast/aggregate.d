503ae47352f1fd2bd859a8c13bf0a6592e1a748f
FeepingCreature <default_357-line@yahoo.de>
2011-12-10 00:08:10 +0100

    Debugging for AggrStatement

--- ast/aggregate.d
+++ ast/aggregate.d
@@ -4,10 +4,14 @@ import ast.base;
 
 class AggrStatement : Statement {
   Statement[] stmts;
+  this(Statement[] stmts) { this.stmts = stmts; }
+  this() { }
   mixin defaultIterate!(stmts);
   override void emitAsm(AsmFile af) {
-    foreach (stmt; stmts)
+    foreach (i, stmt; stmts) {
+      // logln("aggr @", af.currentStackDepth, " [", i, "] ", stmt, " - ", stmts);
       stmt.emitAsm(af);
+    }
   }
   mixin DefaultDup!();
   string toString() { return Format(stmts); }
