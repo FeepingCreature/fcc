20f6efbe84641c2527e1871a7ee7067e56c2e5a7
FeepingCreature <default_357-line@yahoo.de>
2011-12-18 01:33:15 +0100

    Extend unary ops to cover MValues.

--- ast/unary.d
+++ ast/unary.d
@@ -4,32 +4,30 @@ import ast.base, ast.casting, ast.opers, ast.assign, ast.literals, parseBase;
 
 // definitely not an lvalue
 class PrePostOpExpr(bool Post, bool Inc) : Expr {
-  LValue lv;
-  this(LValue lv) {
-    this.lv = lv;
+  Expr ex;
+  this(Expr ex) {
+    this.ex = ex;
   }
   private this() { }
   mixin DefaultDup!();
-  mixin defaultIterate!(lv);
+  mixin defaultIterate!(ex);
   override {
-    IType valueType() {
-      return lv.valueType();
-    }
+    IType valueType() { return ex.valueType(); }
     void emitAsm(AsmFile af) {
-      auto op = lookupOp(Inc?"+":"-", lv, mkInt(1));
+      auto op = lookupOp(Inc?"+":"-", ex, mkInt(1));
       Expr cv;
-      if (lv.valueType() == op.valueType()) cv = op;
+      if (ex.valueType() == op.valueType()) cv = op;
       else {
-        cv = tryConvert(op, lv.valueType());
-        if (!cv) throw new Exception(Format("PrePostOpExpr(", Inc?"+":"-", ") failed: cannot reconvert ", op.valueType(), " to ", lv.valueType()));
+        cv = tryConvert(op, ex.valueType());
+        if (!cv) throw new Exception(Format("PrePostOpExpr(", Inc?"+":"-", ") failed: cannot reconvert ", op.valueType(), " to ", ex.valueType()));
       }
-      auto as = new Assignment(lv, cv);
+      auto as = mkAssignment(ex, cv);
       static if (Post) {
-        lv.emitAsm(af);
+        ex.emitAsm(af);
         as.emitAsm(af);
       } else {
         as.emitAsm(af);
-        lv.emitAsm(af);
+        ex.emitAsm(af);
       }
     }
   }
@@ -40,15 +38,17 @@ Object gotPostIncExpr(ref string text, ParseCb cont, ParseCb rest) {
   auto t2 = text;
   if (!cont(t2, &op)) return null;
   if (t2.accept("++")) {
-    auto lv = fastcast!(LValue)~ op;
-    if (!lv) throw new Exception(Format("Can't post-increment ", op, ": not an lvalue"));
+    auto lv = fastcast!(LValue) (op);
+    auto mv = fastcast!(MValue) (op);
+    if (!lv && !mv) throw new Exception(Format("Can't post-increment ", op, ": not an lvalue or mvalue"));
     text = t2;
-    return new PrePostOpExpr!(true, true)(lv);
+    return new PrePostOpExpr!(true, true)(op);
   } else if (t2.accept("--")) {
     auto lv = fastcast!(LValue)~ op;
-    if (!lv) throw new Exception(Format("Can't post-decrement ", op, ": not an lvalue"));
+    auto mv = fastcast!(MValue) (op);
+    if (!lv && !mv) throw new Exception(Format("Can't post-decrement ", op, ": not an lvalue or mvalue"));
     text = t2;
-    return new PrePostOpExpr!(true, false)(lv);
+    return new PrePostOpExpr!(true, false)(op);
   } else return null;
 }
 mixin DefaultParser!(gotPostIncExpr, "tree.expr.arith.postincdec", "25");
@@ -60,17 +60,19 @@ Object gotPreIncExpr(ref string text, ParseCb cont, ParseCb rest) {
   if (t2.accept("++")) {
     if (!cont(t2, &op))
       t2.failparse("Can't find expression for pre-inc");
-    auto lv = fastcast!(LValue)~ op;
-    if (!lv) text.failparse("Can't pre-increment ", op, ": not an lvalue");
+    auto lv = fastcast!(LValue) (op);
+    auto mv = fastcast!(MValue) (op);
+    if (!lv && !mv) text.failparse("Can't pre-increment ", op, ": not an lvalue or mvalue");
     text = t2;
-    return new PrePostOpExpr!(false, true)(lv);
+    return new PrePostOpExpr!(false, true)(op);
   } else if (t2.accept("--")) {
     if (!cont(t2, &op))
       t2.failparse("Can't find expression for pre-inc");
-    auto lv = fastcast!(LValue)~ op;
-    if (!lv) text.failparse("Can't pre-decrement ", op, ": not an lvalue");
+    auto lv = fastcast!(LValue) (op);
+    auto mv = fastcast!(MValue)~ op;
+    if (!lv && !mv) text.failparse("Can't pre-decrement ", op, ": not an lvalue or mvalue");
     text = t2;
-    return new PrePostOpExpr!(false, false)(lv);
+    return new PrePostOpExpr!(false, false)(op);
   } else return null;
 }
 mixin DefaultParser!(gotPreIncExpr, "tree.expr.arith.preincdec", "26");
