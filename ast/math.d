8fe0721f26e404c9b63eed3863b9b39a7dab3836
FeepingCreature <default_357-line@yahoo.de>
2012-02-26 03:44:19 +0100

    Add operator for unsigned right shift

--- ast/math.d
+++ ast/math.d
@@ -712,13 +712,13 @@ static this() { parsecon.addPrecedence("tree.expr.arith", "12"); }
 const oplist = [
   "+"[], "-", "*", "/",
   "xor", "|", "&", "%",
-  "<<", ">>", "^", "x"
+  "<<", ">>>", ">>", "^", "x"
 ];
 
 const oplevel = [
   0, 1, 2, 2,
   3, 4, 5, 6,
-  7, 7, 8, 8
+  7, 7, 7, 8, 8
 ];
 
 const lvcount = 9;
