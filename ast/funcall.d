5146e59401c9627da0a1a7fc4c2ef7bb29cea4ba
FeepingCreature <default_357-line@yahoo.de>
2012-03-07 13:47:04 +0100

    If the function either has parameters or we couldn't match a terminator, it's not a paramless call.

--- ast/funcall.d
+++ ast/funcall.d
@@ -344,8 +344,9 @@ Object gotCallExpr(ref string text, ParseCb cont, ParseCb rest) {
         t2.failparse("Failed to call function with ", params, ": ", error()._1);
       auto t3 = t2;
       // valid call terminators
-      if (params.length && !t3.acceptTerminatorSoft()) return null;
-    } else text = t2;
+      if (params.length || !t3.acceptTerminatorSoft()) return null;
+    }
+    text = t2;
     return fastcast!(Object) (res);
   };
 }
