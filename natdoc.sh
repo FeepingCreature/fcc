ff9aed70c6b7ea11f5bfa409b0517b41fa9b00eb
FeepingCreature <default_357-line@yahoo.de>
2012-04-18 20:53:36 +0200

    fix natdoc

--- natdoc.sh
+++ natdoc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-EXCLUDES="-xi doc -xi neatspace -xi .git"
+EXCLUDES="-xi doc -xi neatspace -xi .git -xi engine -xi tiffstuff -xi tiff"
 NATDOC="NaturalDocs -i . -o html doc -p doc/project/ $EXCLUDES"
 WATCHES="*.d *.nt ast std nehe"
 while true
