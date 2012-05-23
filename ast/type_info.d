2f09732acb04a3d7f00eec61e64204fae89e9593
FeepingCreature <default_357-line@yahoo.de>
2012-05-23 17:01:10 +0200

    Add type-is class and type-is interface

--- ast/type_info.d
+++ ast/type_info.d
@@ -166,6 +166,8 @@ Object gotTypeIs(ref string text, ParseCb cont, ParseCb rest) {
   string mode;
   if (t2.accept("tuple")) mode = "tuple";
   if (t2.accept("array")) mode = "array";
+  if (t2.accept("class")) mode = "class";
+  if (t2.accept("interface")) mode = "interface";
   if (!mode) t2.failparse("type-is expected type qualifier");
   IType ty;
   if (!rest(t2, "type"[], &ty))
@@ -177,6 +179,8 @@ Object gotTypeIs(ref string text, ParseCb cont, ParseCb rest) {
   switch (mode) {
     case "tuple": res = !!fastcast!(ast.tuples.Tuple) (ty); break;
     case "array": res = !!fastcast!(Array) (ty) || !!fastcast!(ExtArray) (ty); break;
+    case "class": res = !!fastcast!(ClassRef) (ty); break;
+    case "interface": res = !!fastcast!(IntfRef) (ty); break;
   }
   setupStaticBoolLits;
   Expr ex;
