183c809190101b1084b994f8f7b6b18444114be9
FeepingCreature <default_357-line@yahoo.de>
2012-07-14 15:21:20 +0200

    Fix all test cases that are trivial to fix, add a new test for nested class inheritance

---
+++ tests/mktest.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+COUNT=1
+while [ -e "test$COUNT.nt" -o -e "test$COUNTfail.nt" ]; do COUNT=$((COUNT+1)); done
+FILE="test$COUNT.nt"
+echo "module test$COUNT;
+
+void main() {
+}" > $FILE
+joe $FILE
+fcc -run $FILE
--- tests/run.sh
+++ tests/run.sh
@@ -7,8 +7,17 @@ export TESTSUITE="y"
 for file in $(ls *.nt)
 do
   TOTAL=$(($TOTAL + 1))
+  set +m
+  exec 3>&2
+  exec 2>/dev/null
   fcc -run $file >/dev/null
   res=$?
+  exec 2>&3
+  exec 3>&-
+  if [ "$res" -ne "0" ]; then res=1; fi # normalize
+  expectfail=0
+  if [[ "$file" == *fail* ]]; then expectfail=1; fi
+  let res=$res^$expectfail;
   if [ "$res" -eq "0" ]
   then
     PASS=$(($PASS + 1))
@@ -19,5 +28,6 @@ do
     echo -n "!"
   fi
 done
+echo
 echo "Pass: $PASS / $TOTAL"
 echo "Fail: $FAIL / $TOTAL $FAILSTR"
