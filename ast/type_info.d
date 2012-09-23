d003e7acdd990a36bca558f0de3d7f7f6a8c2158
FeepingCreature <default_357-line@yahoo.de>
2012-09-23 19:40:58 +0200

    add pointer to type-is, implement HashMap.remove, tiny std.cgi fix

--- ast/type_info.d
+++ ast/type_info.d
@@ -175,6 +175,7 @@ Object gotTypeIs(ref string text, ParseCb cont, ParseCb rest) {
   if (t2.accept("delegate")) mode = "delegate";
   if (t2.accept("vector")) mode = "vector";
   if (t2.accept("struct")) mode = "struct";
+  if (t2.accept("pointer")) mode = "pointer";
   if (!mode) t2.failparse("type-is expected type qualifier");
   IType ty;
   if (!rest(t2, "type"[], &ty))
@@ -192,6 +193,7 @@ Object gotTypeIs(ref string text, ParseCb cont, ParseCb rest) {
     case "delegate": res = !!fastcast!(Delegate) (ty); break;
     case "vector": res = !!fastcast!(Vector) (ty); break;
     case "struct": res = !!fastcast!(Structure) (ty); break;
+    case "pointer": res = !!fastcast!(Pointer) (ty); break;
   }
   setupStaticBoolLits;
   Expr ex;
