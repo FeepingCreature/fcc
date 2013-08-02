39c6a89cdc5693b1e18769e3aa3aeafcadae1f7c
FeepingCreature <default_357-line@yahoo.de>
2013-08-02 23:17:41 +0200

    when parsing aggregates and offered an out-dg, call it with our parsed statements directly instead of calling it upfront with the aggregator, then filling it in. This fixes an issue where a mixin is wrapped in a static if, causing the static-if to append an aggregator to the surrounding scope, then the mixin to append a _second_ aggregator, which caused the mixin to be executed _after_ the statements in the static-if.

--- ast/aggregate_parse.d
+++ ast/aggregate_parse.d
@@ -2,14 +2,15 @@ module ast.aggregate_parse;
 
 import ast.aggregate, ast.parse, ast.base, ast.scopes, ast.namespace, ast.fun, ast.modules;
 
-AggrStatement parseAggregateBody(ref string text, ParseCb rest, bool error = false, void delegate(Statement) outdg = null) {
+Statement parseAggregateBody(ref string text, ParseCb rest, bool error = false, void delegate(Statement) outdg = null) {
   auto t2 = text;
-  auto as = new AggrStatement;
-  if (outdg) outdg(as);
+  AggrStatement as;
+  // I think this has to be here so that even the first statement is already added to the scope. TODO confirm.
   Statement st;
-  if (t2.many(!!rest(t2, "tree.stmt"[], &st), { as.stmts ~= st; }, "}")) {
+  if (t2.many(!!rest(t2, "tree.stmt"[], &st), { if (outdg) outdg(st); else as.stmts ~= st; }, "}")) {
     text = t2;
-    return as;
+    if (outdg) return Single!(NoOp);
+    else return as;
   }
   else {
     if (error) t2.failparse("Could not parse statement");
@@ -19,7 +20,7 @@ AggrStatement parseAggregateBody(ref string text, ParseCb rest, bool error = fal
 
 Statement parseFullAggregateBody(ref string src, ParseCb rest) {
   auto sc = namespace().get!(Scope);
-  parseAggregateBody(src, rest, true, (Statement st) { sc.addStatement(st); });
+  parseAggregateBody(src, rest, true, &sc.addStatement);
   src = src.mystripl();
   if (src.length) {
     src.failparse("unknown text in aggregate body");
@@ -33,7 +34,7 @@ Object gotAggregateStmt(ref string text, ParseCb cont, ParseCb rest) {
   sc.configPosition(text);
   namespace.set(sc);
   scope(exit) namespace.set(sc.sup);
-  if (auto as = t2.parseAggregateBody(rest, false, (Statement st) { sc._body = st; })) {
+  if (auto as = t2.parseAggregateBody(rest, false, &sc.addStatement)) {
     string tryId;
     if (!t2.accept("}")) {
       auto t3 = t2;
