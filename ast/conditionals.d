d322083baa32cfa02b476843d0cf32c949df1050
FeepingCreature <default_357-line@yahoo.de>
2013-09-08 12:17:59 +0200

    support array-is finally

--- ast/conditionals.d
+++ ast/conditionals.d
@@ -414,7 +414,7 @@ Object gotCompare(ref string text, ParseCb cont, ParseCb rest) {
     auto vt = typeToLLVM(ex1.valueType());
     IType cmptype;
     if (vt == "i32" || vt.endsWith("*")) cmptype = Single!(SysInt);
-    else if (vt == "{i8*, i8*}") cmptype = Single!(Long);
+    else if (vt == "{i8*, i8*}" || vt == "{i32, i8*}") cmptype = Single!(Long);
     else {
       todo(qformat("comparison of ", ex1.valueType(), " ", vt));
     }
