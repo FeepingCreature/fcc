cac065755352b77e9bd48c4501fd36c1f38a9976
FeepingCreature <default_357-line@yahoo.de>
2014-02-03 22:37:56 +0100

    Add -ffunction-sections -fdata-sections to improve --gc-sections effectiveness on linking.

--- fcc.d
+++ fcc.d
@@ -1456,7 +1456,7 @@ string delegate() compile(string file, CompileSettings cs, bool force = false) {
       if (cs.debugModeDwarf) march ~= "-disable-fp-elim ";
       auto llcflags = get_llc_cmd(cs.optimize, cs.debugMode || cs.debugModeDwarf, cs.saveTemps, bogus, bogus);
       // always -O1 the llc, since it's cheap
-      cmdline ~= Format("-o - ", srcname, " |opt "~march~" - "~llcflags~" |llc -O1 "~march~cpumode()~"- -filetype=obj -o ", objname);
+      cmdline ~= Format("-o - ", srcname, " |opt "~march~" - "~llcflags~" |llc -O1 -ffunction-sections -fdata-sections "~march~cpumode()~"- -filetype=obj -o ", objname);
     }
     
     logSmart!(false)("> (", len_parse, "s,", len_gen, "s,", len_emit, "s) ", cmdline);
