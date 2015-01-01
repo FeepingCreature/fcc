c70030610358e23b9a706c9fe910f86f1924d288
FeepingCreature <default_357-line@yahoo.de>
2015-01-01 23:36:57 +0100

    Implement .min/.max for int, short and ubyte in a way that works for aliased types

--- ast/fp.d
+++ ast/fp.d
@@ -2,22 +2,30 @@ module ast.fp;
 
 import parseBase, ast.base, ast.types, ast.literals;
 
-Object gotFloatProperty(ref string text, ParseCb cont, ParseCb rest) {
+Object gotTypeProperty(ref string text, ParseCb cont, ParseCb rest) {
   auto t2 = text;
+  IType ty;
+  if (!rest(t2, "type", &ty)) return null;
   if (!t2.accept(".")) return null;
-  if (t2.accept("infinity"[])) { text = t2; return fastalloc!(FloatExpr)(float.infinity); }
-  if (t2.accept("max"[])) { text = t2; return fastalloc!(FloatExpr)(float.max); }
-  if (t2.accept("min"[])) { text = t2; return fastalloc!(FloatExpr)(float.min); }
-  if (t2.accept("nan"[])) { text = t2; return fastalloc!(FloatExpr)(float.nan); }
-  if (t2.accept("epsilon"[])) { text = t2; return fastalloc!(FloatExpr)(float.epsilon); }
+  if (resolveType(ty) == Single!(Float)) {
+    if (t2.accept("infinity"[])) { text = t2; return fastalloc!(FloatExpr)(float.infinity); }
+    if (t2.accept("max"[])) { text = t2; return fastalloc!(FloatExpr)(float.max); }
+    if (t2.accept("min"[])) { text = t2; return fastalloc!(FloatExpr)(float.min); }
+    if (t2.accept("nan"[])) { text = t2; return fastalloc!(FloatExpr)(float.nan); }
+    if (t2.accept("epsilon"[])) { text = t2; return fastalloc!(FloatExpr)(float.epsilon); }
+  }
+  if (resolveType(ty) == Single!(SysInt)) {
+    if (t2.accept("max"[])) { text = t2; return fastalloc!(IntExpr)(int.max); }
+    if (t2.accept("min"[])) { text = t2; return fastalloc!(IntExpr)(int.min); }
+  }
+  if (resolveType(ty) == Single!(Short)) {
+    if (t2.accept("max"[])) { text = t2; return fastalloc!(IntExpr)(short.max); }
+    if (t2.accept("min"[])) { text = t2; return fastalloc!(IntExpr)(short.min); }
+  }
+  if (resolveType(ty) == Single!(UByte)) {
+    if (t2.accept("max"[])) { text = t2; return fastalloc!(IntExpr)(ubyte.max); }
+    if (t2.accept("min"[])) { text = t2; return fastalloc!(IntExpr)(ubyte.min); }
+  }
   return null;
 }
-mixin DefaultParser!(gotFloatProperty, "tree.expr.fprop", "23030", "float");
-
-Object gotIntProperty(ref string text, ParseCb cont, ParseCb rest) {
-  auto t2 = text;
-  if (!t2.accept(".")) return null;
-  if (t2.accept("max"[])) { text = t2; return fastalloc!(IntExpr)(int.max); }
-  return null;
-}
-mixin DefaultParser!(gotIntProperty, "tree.expr.iprop", "23031", "int");
+mixin DefaultParser!(gotTypeProperty, "tree.expr.tprop", "23030");
